92e1916fd4556a9bc8c32e518fbe4c3a569036d9
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-09-19 19:17:12 -0400

    added basic height and width to button; added initial scaffolding for responsive design

--- src/components/Add.svelte
+++ src/components/Add.svelte
@@ -6,4 +6,30 @@
 
 <style>
 
+    button {
+        width: 100%;
+        height: 2rem;
+        cursor: pointer;
+    }
+    
+    @media only screen and (max-width:600px) {
+
+    }
+
+    @media only screen and (min-width:600px) {
+
+    }
+
+    @media only screen and (min-width:600px) {
+
+    }
+
+    @media only screen and (min-width:600px) {
+
+    }
+
+    @media only screen and (min-width:600px) {
+
+    }  
+
 </style>
