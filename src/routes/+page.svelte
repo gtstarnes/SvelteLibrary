7277299d7d124da01dddad59e9add076ed444222
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-09-19 19:07:42 -0400

    added Add button to home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
-
+    // component imports
+    import Add from "../components/Add.svelte";
 </script>
 
 <main>
     <h1>Welcome to Home</h1>
+    <Add />
 </main>
 
 
 <style>
-    
+
 </style>
 
