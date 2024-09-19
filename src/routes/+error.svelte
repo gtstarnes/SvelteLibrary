3009ff4906b9ef1fe476e898a603eb3cf76a4daa
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-09-19 19:01:48 -0400

    added page status and page error message to error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,7 +1,11 @@
-<script lang="ts"></script>
+<script lang="ts">
+    import {page} from '$app/stores'
+	import { error } from '@sveltejs/kit';
+</script>
 
 
 <div>
     <h1>Oops. There was an error.</h1>
+    <h3>{$page.status}: {$page.error?.message}</h3>
     <div><a href="/">Go back Home</a></div>
 </div>
