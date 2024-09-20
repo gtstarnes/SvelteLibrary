2a2968862957b746cba15774b9fecbfb36419823
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-09-19 20:10:32 -0400

    added mediaInfo component for testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
     // component imports
     import Add from "../components/Add.svelte";
+	import MediaInfo from "../components/MediaInfo.svelte";
 </script>
 
 <main>
     <h1>Welcome to Home</h1>
     <Add message={"Add media"} />
+    <MediaInfo />
 </main>
 
 
