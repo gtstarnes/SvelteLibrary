88b5ccd8c45bde813469b7d70635159c1104b0e3
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-09-20 20:20:39 -0400

    added default message before media selection

--- src/components/MediaForm.svelte
+++ src/components/MediaForm.svelte
@@ -1,11 +1,22 @@
-<script lang="ts"></script>
+<script lang="ts">
+
+    export let mediaType = "Book"
+</script>
 
 
 <div>
+    {#if mediaType === ""}
+        <div>Please Select a type of media</div>
+    {:else}
     <div>
         <label for="title">Title:</label>
-        <input type="text" />
+        <input type="text" required />
     </div>
+    <div>
+        <label for="year">Release Year:</label>
+        <input type="number" max=2024 />
+    </div>
+    {/if}
 </div>
 
 
