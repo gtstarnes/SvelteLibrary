3b096f749a40b60a323569abc75b28051a0e05d3
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-09-20 20:20:24 -0400

    removed form / added mediaForm component

--- src/components/MediaInfo.svelte
+++ src/components/MediaInfo.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     //component imports
     import Add from "./Add.svelte";
+	import MediaForm from "./MediaForm.svelte";
 
     let mediaType = "";
 </script>
@@ -20,65 +21,7 @@
                 <option value="Board Game">Board Game</option>
             </select>
         </div>
-        <div>
-            <label for="title">Title: </label>
-            <input id="title" name="title" type="text" />
-        </div>
-        <div>
-            <label for="creator">Creator: </label>
-            <input id="creator" name="creator" type="text" />
-        </div>
-
-        <div>
-            <label for="genre">Genre: </label>
-            <fieldset>
-                {#if mediaType === "TV Show" || mediaType === "Movie"}
-                <div>
-                    <input type="checkbox" name="ActionAdventure" id="ActionAdventure" />
-                    <lable for="ActionAdventure">Action / Adventure</lable>
-                    <input type="checkbox" name="Horror" id="Horror" />
-                    <lable for="Horror">Horror</lable>
-                    <input type="checkbox" name="Romance" id="Romance" />
-                    <lable for="Romance">Romance</lable>
-                    <input type="checkbox" name="Thriller" id="Thriller" />
-                    <lable for="Thriller">Thriller</lable>
-                    <input type="checkbox" name="Historical" id="Historical" />
-                    <lable for="Historical">Historical</lable>
-                    <input type="checkbox" name="Historical Fiction" id="Historical Fiction" />
-                    <lable for="Historical Fiction">Historical Fiction</lable>
-                    <input type="checkbox" name="Drama" id="Drama" />
-                    <lable for="Drama">Drama</lable>
-                    <input type="checkbox" name="ComicManga" id="ComicManga" />
-                    <lable for="ComicManga">Animation / Anime</lable>
-                    <input type="checkbox" name="Fantasy" id="Fantasy" />
-                    <lable for="Fantasy">Fantasy</lable>
-                </div>
-                {:else if mediaType === "Album"}
-                    <div>
-                        <input type="checkbox" name="classical" id="classical" />
-                        <label for="classical">Classical</label>
-                        <input type="checkbox" name="Rock" id="Rock" />
-                        <label for="Rock">Rock</label>
-                        <input type="checkbox" name="Metal" id="Metal" />
-                        <label for="Metal">Metal</label>
-                        <input type="checkbox" name="Jazz" id="Jazz" />
-                        <label for="Jazz">Jazz</label>
-                        <input type="checkbox" name="Country" id="Country" />
-                        <label for="Country">Country</label>
-                        <input type="checkbox" name="Folk" id="Folk" />
-                        <label for="Folk">Folk</label>
-                        <input type="checkbox" name="Rap / Hip Hop" id="Rap / Hip Hop" />
-                        <label for="Rap / Hip Hop">Rap / Hip Hop</label>
-                        <input type="checkbox" name="Electronic" id="Electronic" />
-                        <label for="Electronic">Electronic</label>
-                    </div>
-                {/if}
-            </fieldset>
-        </div>
-        <div>
-            <label for="">Release Year</label>
-            <input type="number" />
-        </div>
+        <MediaForm {mediaType} />
     </form>
     <div>
         <Add message="Add Media" />
