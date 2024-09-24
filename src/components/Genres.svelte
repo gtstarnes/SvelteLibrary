7388182f1e83ba6b103f660d9418b196fd3b0b1c
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-09-23 20:47:23 -0400

    added conditional rendering and for/each loop

--- src/components/Genres.svelte
+++ src/components/Genres.svelte
@@ -1,12 +1,24 @@
 <script lang="ts">
+    // object imports
+    import { genres } from "../objects/genres";
+    export let mediaType;
 
+    $: media = genres[mediaType];
 </script>
 
 <div class="container">
-    <div>
-        
-    </div>
+    {#if mediaType === ""}
+        <div></div>
+    {:else}
+        {#each media as genre (genre)}
+            <div>
+                <input type="checkbox" id={genre} name="genre" />
+                <label for={genre}>{genre}</label>
+            </div>
+        {/each}
+    {/if}
 </div>
+
 
 <style>
 
