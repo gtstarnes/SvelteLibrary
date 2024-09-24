3c2288a27c4dc40a4dc735ddf04c90f5c1b8fb17
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-09-23 20:22:51 -0400

    created object with genres

---
+++ src/objects/genres.ts
@@ -0,0 +1,11 @@
+export const genres = {
+    books: ["Non-Fiction", "Poetry", "Science Fiction", 
+        "Fantasy", "Fiction", "True Crime", "History", "Philosophy", 
+        "Romance", "Vampire Romance", "Graphic Novel", "Manga",],
+    movies: ["Action", "Comedy", "Drama", "Horror", "Mafia", "Mystery",
+        "Martial Arts", "Silent", "Sports", "Superhero", "Thriller", "Western",],
+    music:  ["Ambient", "Classical", "Country", "Dance", "Disco", "Electronic", "Folk",
+         "Heavy Metal", "Latin", "Pop", "Rock", "World",],
+    games: ["Action / Adventure", "Dance", "Fighting", "FPS", "Horror", "MMO", "Platformer",
+         "Puzzle", "RTS", "Shooter-Looter", "Stealth", "Survival",],
+}
