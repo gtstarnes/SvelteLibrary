6058df7b5dc20f0f1d7ef00fff51c1be369eb499
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-09-23 21:01:08 -0400

    removed if/else statement

--- src/components/Genres.svelte
+++ src/components/Genres.svelte
@@ -1,22 +1,18 @@
 <script lang="ts">
     // object imports
     import { genres } from "../objects/genres";
-    export let mediaType;
+    export let mediaType: keyof typeof genres;
 
     $: media = genres[mediaType];
 </script>
 
 <div class="container">
-    {#if mediaType === ""}
-        <div></div>
-    {:else}
         {#each media as genre (genre)}
             <div class="item">
                 <input type="checkbox" id={genre} name="genre" />
                 <label for={genre}>{genre}</label>
             </div>
         {/each}
-    {/if}
 </div>
 
 
