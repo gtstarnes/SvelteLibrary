fec26c30232299318511b82d78c9e1a1f549c4aa
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-10-03 23:29:10 -0400

    added id key to library type

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,7 @@
 export type MediaType = "Book" | "Album" | "Movie" | "TV Show" | "Video Game" | ""
 
 export type LibraryItem = {
+    id: number,
     medium: string,
     title: string,
     release_year: number,
